c9004ccb8f5b109091c67a1b7ccb8a25738acae9
FrancisPoeGSA <francis.poe@gsa.gov>
2022-10-03 13:23:03 -0400

    Updated with the amendments from FAC 2022-05 effective October 25, 2022.

--- ReportValidationReports/4. MultiLevelValidation-TransformErrorReport2HTML.bat
+++
@@ -1,4 +0,0 @@
-cd..
-cd dita
-cd out
-C:\GSA-Acquisition-Tools\TOOLS\DITA-OT\dita-ot-2.5.4\bin\dita  -i "C:\GSA_Regs\GSA-Acquisition-FAR\dita\out\ErrorReport.dita" -f html5 -o "C:\GSA_Regs\GSA-Acquisition-FAR\dita\out"
